5880b8e3cf4bf78a780cf94d975d75bd9c054da5
LynnNguyen3219 <lynnnguyen3219@gmail.com>
2026-01-29 08:38:41 -0800

    cowabunga

--- pygame26.py
+++ pygame26.py
@@ -8,29 +8,52 @@ pygame.init()
 pygame.display.set_caption('cowabunga!')
 monitor_size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
 
-win_size = (400, 400)
+win_size = (1920, 1080)
 
-screen = pygame.display.set_mode(win_size, pygame.RESIZABLE)
+screen = pygame.display.set_mode(win_size, 0, 32)
+
+aigis = pygame.image.load('aigis.png')
+
+moving_right = False
+moving_left = False
+
+aigis_location = [50,50]
+aigis_vertical_location = 0
 
 fullscreen = False
 
 while True:
+    screen.fill((255,255,255))
+
+    screen.blit(aigis,aigis_location)
+
+    if aigis_location[1] > win_size[1]-aigis.get_height():
+        aigis_vertical_location = -aigis_vertical_location
+    else:
+        aigis_vertical_location += 0.1
+    aigis_location[1] += aigis_vertical_location
+
+    if moving_right == True:
+        aigis_location[0] += 4
+    if moving_left == True:
+        aigis_location[0] -= 4
 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
-    if event.type == VIDEORESIZE:
-        if not fullscreen:
-            screen = pygame.display.set_mode(event.size, pygame.RESIZABLE)
+    
     pygame.display.update()
     if event.type == KEYDOWN:
-        if event.key == K_f:
-            fullscreen = not fullscreen
-        if fullscreen:
-            screen = pygame.display.set_mode(monitor_size, pygame.FULLSCREEN)
-        else:
-            screen = pygame.display.set_mode((screen.get_width(), screen.get_height()), pygame.RESIZABLE)
+        if event.key == K_RIGHT:
+            moving_right = True
+        if event.key == K_LEFT:
+            moving_left = True
+    if event.type == KEYUP:
+        if event.key == K_RIGHT:
+            moving_right = False
+        if event.key == K_LEFT:
+            moving_left = False 
 
 pygame.display.update()
 clock.tick(60)
